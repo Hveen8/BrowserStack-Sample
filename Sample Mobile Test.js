8c8e6be44945c1e54b990e30aed1e4021190bccb
Hveen8 <93657721+Hveen8@users.noreply.github.com>
2021-11-04 13:12:07 -0400

    Create Sample Mobile Test.js

---
+++ Sample Mobile Test.js
@@ -0,0 +1,53 @@
+exports.config = {
+  user: process.env.BROWSERSTACK_USERNAME || 'BROWSERSTACK_USERNAME',
+  key: process.env.BROWSERSTACK_ACCESS_KEY || 'BROWSERSTACK_ACCESS_KEY',
+
+  updateJob: false,
+  specs: [
+    './examples/run-first-test/specs/first_test.js'
+  ],
+  exclude: [],
+
+  capabilities: [{
+    project: "First Webdriverio Android Project",
+    build: 'Webdriverio Android',
+    name: 'first_test',
+    device: 'Google Pixel 3',
+    os_version: "9.0",
+    app: process.env.BROWSERSTACK_APP_ID || 'bs://<hashed app-id>',
+    'browserstack.debug': true
+  }],
+
+  logLevel: 'info',
+  coloredLogs: true,
+  screenshotPath: './errorShots/',
+  baseUrl: '',
+  waitforTimeout: 10000,
+  connectionRetryTimeout: 90000,
+  connectionRetryCount: 3,
+
+  framework: 'mocha',
+  mochaOpts: {
+    ui: 'bdd',
+    timeout: 20000
+  }
+};
+
+var assert = require('assert');
+
+describe('Search Wikipedia Functionality', () => {
+  it('can find search results', async () => {
+    var searchSelector = await $(`~Search Wikipedia`);
+    await searchSelector.waitForDisplayed({ timeout: 30000 });
+    await searchSelector.click();
+
+    var insertTextSelector = await $('android=new UiSelector().resourceId("org.wikipedia.alpha:id/search_src_text")');
+    await insertTextSelector.waitForDisplayed({ timeout: 30000 });
+
+    await insertTextSelector.addValue("Browsertack");
+    await browser.pause(5000);
+
+    var allProductsName = await $$(`android.widget.TextView`);
+    assert(allProductsName.length > 0);
+  });
+});
