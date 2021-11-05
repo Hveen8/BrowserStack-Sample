9c61785af08d325b475b9fadbca92aa0b5adf19b
Hveen8 <93657721+Hveen8@users.noreply.github.com>
2021-11-05 09:26:47 -0400

    Update sampleMobileTest.js

--- sampleMobileTest.js
+++ sampleMobileTest.js
@@ -1,18 +1,19 @@
 var assert = require('assert');
+var webdriver = require('selenium-webdriver');
 
 describe('Search Wikipedia Functionality', () => {
   it('can find search results', async () => {
-    var searchSelector = await $(`~Search Wikipedia`);
+    var searchSelector = await webdriver.$(`~Search Wikipedia`);
     await searchSelector.waitForDisplayed({ timeout: 30000 });
     await searchSelector.click();
 
-    var insertTextSelector = await $('android=new UiSelector().resourceId("org.wikipedia.alpha:id/search_src_text")');
+    var insertTextSelector = await webdriver.$('android=new UiSelector().resourceId("org.wikipedia.alpha:id/search_src_text")');
     await insertTextSelector.waitForDisplayed({ timeout: 30000 });
 
     await insertTextSelector.addValue("Browsertack");
     await browser.pause(5000);
 
-    var allProductsName = await $$(`android.widget.TextView`);
+    var allProductsName = await webdriver.$$(`android.widget.TextView`);
     assert(allProductsName.length > 0);
   });
 });
